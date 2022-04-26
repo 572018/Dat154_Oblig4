dbc58ff4e4e3b803cd861b612a5cbc8573457af2
Emma <ehellandhansen@gmail.com>
2022-04-26 16:30:17 +0200

    Ta i bruk bibliotek

---
+++ Dat154_Oblig4_Library/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dat154_Oblig4_Library
+{
+    public partial class Customer
+    {
+        public string Email { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Password { get; set; } = null!;
+
+        public virtual ICollection<Booking> Bookings { get; set; }
+
+        public Customer(string emailAddress, string name, string password)
+        {
+            Email = emailAddress;
+            Name = name;
+            Password = password;
+        }
+    }
+}
