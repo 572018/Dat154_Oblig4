dcb84190fad44d9d2a2c48f581513e081a37c605
Jan William Jensen <69847071+572018@users.noreply.github.com>
2022-04-26 11:19:43 +0200

    Create Employe.cs

---
+++ CleaningSystem/Entities/Employe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleaningSystem.Entities
+{
+    internal class Employe
+    {
+        public int Room { get; set; }
+        public bool CheckedIn { get; set; }
+        public string? Cleaning { get; set; }
+        public string? Service { get; set; }
+        public string? Maintenance { get; set; }
+
+    }
+}
