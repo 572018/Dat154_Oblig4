293f5a95cfb233086006db93fe2fc54d76d01c17
Emma <ehellandhansen@gmail.com>
2022-04-26 11:16:26 +0200

    Nytt prosjekt -  BookingSystem (MVC) Opprettet RoomModel

---
+++ BookingSystem/Models/RoomModel.cs
@@ -0,0 +1,20 @@
+﻿namespace BookingSystem.Models
+{
+    public class RoomModel
+    {
+
+        public int RoomNumber { get; set; }
+        public int NumberOfRooms { get; set; }
+        public RoomSize RoomSize { get; set; }
+
+        public Boolean NiceView { get; set; } // ?
+
+        public int PricePerNight { get; set; } // ?
+
+    }
+
+    public enum RoomSize
+    {
+
+    }
+}
